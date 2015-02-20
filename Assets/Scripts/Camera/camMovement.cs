fa1afb623f4390716b5c7e5b3d03f6158a9109b0
Max Harlynking <mharlynk@andrew.cmu.edu>
2015-02-20 16:35:08 -0500

    Created initial code and basic movement

---
+++ Assets/Scripts/Camera/camMovement.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class camMovement : MonoBehaviour {
+
+	public Transform player;
+	Vector3 newPos;
+	public float speed = 40.0f;
+
+	void Start () {
+		player = GameObject.Find ("Player").transform;
+		}
+
+	void Update () {
+		newPos = new Vector3 (player.position.x, player.position.y, transform.position.z);
+		transform.position = Vector3.MoveTowards (transform.position, newPos, speed * Time.deltaTime);
+	}
+}
---
+++ Assets/Scripts/Character/touchMovement.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class touchMovement : MonoBehaviour {
+	
+	Vector2 startPos;
+	float xMove;
+	float yMove;
+	public float touchRadius = 30f;
+	public float maxSpeed = 5f;
+	Vector2 speed;
+	Vector2 lastLoc;
+
+
+	// Update is called once per frame
+	void Update () {
+				if (Input.touchCount > 0 && Input.GetTouch (0).phase == TouchPhase.Began) {
+					startPos = Input.GetTouch (0).position;
+					lastLoc = Input.GetTouch (0).position;
+				} else if (Input.touchCount > 0 && Vector2.Distance (lastLoc, Input.GetTouch (0).position) > touchRadius * 5) {
+					startPos = Input.GetTouch (0).position;
+					lastLoc = Input.GetTouch (0).position;
+				} else if (Input.touchCount > 0 && Input.GetTouch (0).phase != TouchPhase.Ended) {
+					xMove = (Input.GetTouch (0).position.x - startPos.x) / touchRadius;
+					if (xMove > 1f) xMove = 1f;
+					yMove = (Input.GetTouch (0).position.y - startPos.y) / touchRadius;
+					if (yMove > 1f) yMove = 1f;
+					speed = new Vector2 (xMove * maxSpeed, yMove * maxSpeed);
+					rigidbody2D.MovePosition (rigidbody2D.position + speed * Time.deltaTime);
+					lastLoc = Input.GetTouch (0).position;
+				} else if (Input.touchCount == 0) {
+					startPos = new Vector2();
+				}
+		}
+}
